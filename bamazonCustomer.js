9aae781e8142e49c00e2394d3c02ac3aa5ad494f
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-13 11:35:28 -0700

    NPM installed and readproduct function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,4 @@
-const inquirer = require("inquirer");
+// const inquirer = require("inquirer");
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
@@ -12,4 +12,15 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
+    readProducts();
 });
+
+function readProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+}
