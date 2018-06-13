15a21b62b98396d72b9fbaa1c331c21b8ea476a3
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-13 13:21:04 -0700

    fixed function and console.log

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,55 +3,53 @@ const inquirer = require("inquirer");
 const mysql = require("mysql");
 //Server information
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root",
-    database: "bamazon"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon"
 });
 //Action when connected to server.
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    readProducts();
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  readProducts();
 });
 //Displays all the products for sale in a list.
 function readProducts() {
   console.log("Displaying all products...");
-  connection.query("SELECT * FROM products", function(err, res) {
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     for (let i = 0; i < res.length; i++) {
-        console.log("\n" + res[i].id + 
-          ". " + res[i].product_name + 
-          "\nPrice: $" + res[i].price);
+      console.log("\n" + res[i].id +
+        ". " + res[i].product_name +
+        "\nPrice: $" + res[i].price);
     }
-    
+
     startInquirer(res);
 
   });
 }
 
-function startInquirer(x){
+function startInquirer(x) {
   inquirer.prompt([
     {
-        type: 'input',
-        name: 'product',
-        message: 'Which product would you like to purchase?(Enter product #)'
+      type: 'input',
+      name: 'product',
+      message: 'Which product would you like to purchase?(Enter product #)'
     },
     {
       type: 'input',
       name: 'quantity',
       message: 'Enter the amount you would like to purchase?'
     }
-    ]).then(function (inquirerResults) {
-        console.log(inquirerResults.product);
-        console.log(inquirerResults.quantity);
-        let query = "UPDATE products SET quantity = quantity - ? WHERE id = ?"
-        connection.query(query, [inquirerResults.quantity, x[inquirerResults.product - 1].id], function (err, res) {
-                        if (err) throw err;
-                        console.log("Database Successfully updated");
-                    });
-        connection.end();
-    }); 
+  ]).then(function (inquirerResults) {
+    let query = "UPDATE products SET quantity = quantity - ? WHERE id = ?"
+    connection.query(query, [inquirerResults.quantity, x[inquirerResults.product - 1].id], function (err, res) {
+      if (err) throw err; 
+      console.log("Successfully purchased " + inquirerResults.quantity + ' copy/copies of ' + x[inquirerResults.product - 1].product_name + '.');
+    });
+    connection.end();
+  });
 
 }
